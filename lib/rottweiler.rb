acd5e2b465dfc2a12bf17d8584e3b972cd003030
Groza Sergiu <groza@opinioni.net>
2023-02-17 16:54:31 +0100

    Initial commit

---
+++ lib/rottweiler.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "rottweiler/version"
+
+module Rottweiler
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/rottweiler/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Rottweiler
+  VERSION = "0.1.0"
+end
