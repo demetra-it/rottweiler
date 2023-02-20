bb4d8981a3624a38576d47602511eded2a2aaf02
Groza Sergiu <groza@opinioni.net>
2023-02-20 17:48:58 +0100

    NEW: implemented `Rottweiler::InvalidParamsError`

--- lib/rottweiler.rb
+++ lib/rottweiler.rb
@@ -2,18 +2,24 @@
 
 require_relative 'rottweiler/version'
 require_relative 'rottweiler/configuration'
+require_relative 'rottweiler/authentication'
 
 # Rottweiler is a Ruby gem for easy verification of JSON Web Tokens (JWTs) in Rails applications.
 module Rottweiler
   class Error < StandardError; end
+  class InvalidParamsError < StandardError; end
 
-  # Configuration:
-  #
-  #  Rottweiler.config do |config|
-  #    config.token_header = 'Authorization'
-  #    config.token_param = [:token]
-  # end
   class << self
+    # Allows to configure Rottweiler gem:
+    #
+    #   Rottweiler.config do |config|
+    #     config.token_header = 'Authorization'
+    #     config.token_param = [:token]
+    #
+    #     config.jwt.algorithm = 'RS256'
+    #     config.jwt.decode_key = '--- RSA PUBLIC KEY ---'
+    #   end
+    #
     def config(&block)
       config_instance = Configuration.instance
 
